4e8db822da8fdf99418be38720f2c0f66354a17e
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-12-04 10:47:01 -0300

    agrega generacion de json

--- generar_json.js
+++ generar_json.js
@@ -3,29 +3,39 @@ const csv = require('csvtojson')
 const { SERVICIOS_SALUD } = require('./constantes')
 const { PATH_WEBAPP } = require('./config')
 
-const leerPronostico = servicio => {
-  csv()
-    .fromFile(`./r/pronosticos/${servicio}.csv`)
-    .then(json => {
-    })
-    .catch(err => console.log(servicio))
+const leerHistorico = async año => {
+  return csv().fromFile(`./scrapes/datos_${año}.csv`)
 }
 
-const datos = []
+const leerPronostico = async servicio => {
+  return csv().fromFile(`./r/pronosticos/${servicio}.csv`)
+}
 
-SERVICIOS_SALUD.forEach(servicio => {
-  let historico = []
-  let pronostico = leerPronostico(servicio)
-  datos.push({
-    servicio,
-    historico,
-    pronostico
+const generarJSON = async () => {
+  const datos = []
+  await Promise.all(
+    SERVICIOS_SALUD.map(async servicio => {
+      let historico2017 = await leerHistorico(2017)
+      let historico2018 = await leerHistorico(2018)
+      let historico2019 = await leerHistorico(2019)
+      let pronostico = await leerPronostico(servicio)
+      datos.push({
+        servicio,
+        historico: {
+          2017: historico2017.find(s => s.Servicio === servicio),
+          2018: historico2018.find(s => s.Servicio === servicio),
+          2019: historico2019.find(s => s.Servicio === servicio)
+        },
+        pronostico: pronostico.map(p => Math.round(Number(p['Point Forecast'])))
+      })
+    })
+  )
+  fs.writeFile("output.json", JSON.stringify(datos), 'utf8', err => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('Archivo JSON guardado');
   })
-})
+}
 
-fs.writeFile("output.json", JSON.stringify(datos), 'utf8', err => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log('Archivo JSON guardado');
-})
+generarJSON()
