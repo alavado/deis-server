797d68e9eeade54f78b84710e33113b024f0e304
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-03-15 10:51:06 -0300

    actualizacion 11/2020

--- generar_json.js
+++ generar_json.js
@@ -13,23 +13,26 @@ const leerPronostico = async servicio => {
 
 const formatearJSON = datos => {
   const llaves = Object.keys(datos)
-  return llaves.filter(k => !isNaN(k)).sort((x, y) => Number(x) > Number(y) ? 1 : -1).map(k => Number(datos[k]))
+  return llaves
+    .filter(k => !isNaN(k))
+    .sort((x, y) => Number(x) > Number(y) ? 1 : -1)
+    .map(k => Number(datos[k]))
 }
 
 const generarJSON = async () => {
   const datos = []
   await Promise.all(
     SERVICIOS_SALUD.map(async servicio => {
-      let historico2017 = await leerHistorico(2017)
       let historico2018 = await leerHistorico(2018)
       let historico2019 = await leerHistorico(2019)
+      let historico2020 = await leerHistorico(2020)
       let pronostico = await leerPronostico(servicio)
       datos.push({
         servicio,
         historico: {
-          2017: formatearJSON(historico2017.find(s => s.Servicio === servicio)),
           2018: formatearJSON(historico2018.find(s => s.Servicio === servicio)),
-          2019: formatearJSON(historico2019.find(s => s.Servicio === servicio))
+          2019: formatearJSON(historico2019.find(s => s.Servicio === servicio)),
+          2020: formatearJSON(historico2020.find(s => s.Servicio === servicio))
         },
         pronostico: pronostico.map(p => Math.round(Number(p['Point Forecast'])))
       })
